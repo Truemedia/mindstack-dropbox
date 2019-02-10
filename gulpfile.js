c17637c318dc228e90b59ccaf0d1973bf0e606c6
Wade Penistone <wadespackages@yahoo.com>
2019-02-10 16:42:23 +0000

    File watcher

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const Dropbox = require('dropbox').Dropbox;
+const fs = require('fs');
+const path = require('path');
+const fetch = require('node-fetch');
+const {watch} = require('gulp');
+
+let accessToken = process.env['DROPBOX_OAUTH_TOKEN']; // OAuth generated token. See app page for generate token button
+var dbx = new Dropbox({accessToken, fetch});
+
+let txtGlob = './*.txt';
+let imgGlob = './*.jpg';
+
+const watcher = watch([txtGlob, imgGlob], {events: 'add'});
+
+// Move added files to dropbox
+watcher.on('add', function(filePath, stats) {
+    let cloudPath = `/${filePath}`;
+    fs.readFile(path.join(__dirname, cloudPath), 'utf8', function (err, contents) {
+      if (err) { console.log('Error: ', err); }
+
+      // This uploads test.txt to the root of your dropbox
+      dbx.filesUpload({ path: cloudPath, contents: contents })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    });
+});
